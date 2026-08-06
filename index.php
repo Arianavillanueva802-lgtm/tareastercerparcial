4fb82bf8b7a0bf27632e304f5683e6ff83cbb078
Ariana <villanuevaariana802@gmail.com>
2026-08-06 14:29:16 -0600

    Archivos de tailwindcss

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud de Tareas</title>
+    <link rel="stylesheet" href="assets/css/src/output.css">   
 </head>
 <body>
-    
+    <h1 class="text-2xl font-bold bg-amber-400 text-center py-4 text-light">control de tareas</h1>
 </body>
 </html>
